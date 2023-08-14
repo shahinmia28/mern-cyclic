821f1a93cccc0a067e2780c3377d44a24b257edd
Shahin Mia <shahin01777296933@gmail.com>
2023-08-15 00:09:43 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
 app.listen(3001, async () => {
-  console.log(`server is running at http://localhost:3001`);
+  console.log(`server is running`);
   await connectDB();
 });
