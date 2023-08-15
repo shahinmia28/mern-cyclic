e8a6f09c87a4bf00fae836ccc910ed5745146942
Shahin Mia <shahin01777296933@gmail.com>
2023-08-15 23:34:59 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
+const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const PORT = process.env.PORT || 3001;
