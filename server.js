6f385d49e734bd16642ab30c45d4f38ad4338b16
Shahin Mia <shahin01777296933@gmail.com>
2023-08-15 23:26:31 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
+dotenv.config({ path: "./config.env" });
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, async () => {
+  console.log(`server is running at http://localhost:3001`);
+  await connectDB();
+});
