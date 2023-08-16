97162e2f10a187a295252d98d5d07b7228f46fcc
Shahin Mia <shahin01777296933@gmail.com>
2023-08-16 23:16:01 +0600

    Update index.jsdddd

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.use("*", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "../buildindex.html"));
 });
 
