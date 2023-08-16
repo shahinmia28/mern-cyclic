f8a09ccd0f8fc46eebf1aae896ee0443b2d4f0ab
Shahin Mia <shahin01777296933@gmail.com>
2023-08-16 23:13:03 +0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+const seedRouter = require("./routers/seedData");
+const userRouter = require("./routers/userRouter");
+const orderRouter = require("./routers/orderRouter");
+
+const path = require("path");
+const connectDB = require("../src/config/db");
+
+require("dotenv").config({ path: "./config.env" });
+
+const PORT = process.env.PORT || 3001;
+
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../client/public")));
+
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
+
+app.use("/seed", seedRouter);
+app.use("/user", userRouter);
+app.use("/user", orderRouter);
+
+app.listen(PORT, async () => {
+  console.log(`server is running at http://localhost:3001`);
+  await connectDB();
+});
