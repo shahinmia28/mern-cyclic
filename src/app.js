96b46e44e4cc79d05710ea379913c92e56327dff
Shahin Mia <shahin01777296933@gmail.com>
2023-08-16 22:15:48 +0600

    Update app.jsaa

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const seedRouter = require("./routers/seedData");
 const userRouter = require("./routers/userRouter");
 const app = express();
 const orderRouter = require("./routers/orderRouter");
-const path = require("path");
+
 
 const cors = require("cors");
 app.use(cors());
@@ -11,11 +11,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(__dirname, "build"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build/index.html"));
-});
 
 app.use("/seed", seedRouter);
 app.use("/user", userRouter);
