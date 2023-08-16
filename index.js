86662c0a74e1870c323d45bd6f4e57eca2a22598
Shahin Mia <shahin01777296933@gmail.com>
2023-08-16 23:39:14 +0600

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.use("/user", userRouter);
 app.use("/user", orderRouter);
 
 app.listen(PORT, async () => {
-  console.log(`server is running at http://localhost:3001`);
+  console.log(`server is running`);
   await connectDB();
 });
