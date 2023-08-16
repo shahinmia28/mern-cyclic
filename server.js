2108d21d9f47387f800c9eb836900a0959f3deb3
Shahin Mia <shahin01777296933@gmail.com>
2023-08-16 22:16:52 +0600

    Update server.jsff

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 require("dotenv").config();
-
-
 const PORT = process.env.PORT || 3001;
+const path = require("path");
+
+app.use(express.static(__dirname, "build"));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "build/index.html"));
+// });
 
 app.listen(PORT, async () => {
   console.log(`server is running`);
