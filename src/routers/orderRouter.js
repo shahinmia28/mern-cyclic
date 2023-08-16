9178a3428489abb75ef3902cf4dfc43f563f3e59
Shahin Mia <shahin01777296933@gmail.com>
2023-08-16 23:31:16 +0600

    Delete src directory

--- src/routers/orderRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { orderData, myOrderData } = require("../controllers/orderController");
-
-const orderRouter = express.Router();
-
-orderRouter.post("/order_data", orderData);
-orderRouter.post("/myorderdata", myOrderData);
-module.exports = orderRouter;
