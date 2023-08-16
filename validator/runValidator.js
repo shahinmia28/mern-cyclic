ea31ed2d404e27bfb8c15a5cc2f17dfd92a7d59f
Shahin Mia <shahin01777296933@gmail.com>
2023-08-16 23:34:01 +0600

    Add files via upload

---
+++ validator/runValidator.js
@@ -0,0 +1,18 @@
+const { validationResult } = require("express-validator");
+
+const runValidation = (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        success: false,
+        message: errors.array()[0].msg,
+      });
+    }
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+};
+
+module.exports = runValidation;
