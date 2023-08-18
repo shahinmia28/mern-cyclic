accc64847657888b29fc6ec09dc1704ee05486c7
Shahin Mia <shahin01777296933@gmail.com>
2023-08-18 21:19:12 +0600

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "./build")));
+app.use(express.static(__dirname, "./build"));
 
 app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./build/index.html"));
+  res.sendFile(path.resolve(__dirname, "./build/index.html"));
 });
 
 // app.get("/", (req, res) => {
